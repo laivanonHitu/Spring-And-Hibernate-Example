c06bf09b39d0cff8580760ac417dd3fdb096817c
laivanonHitu <71056130+laivanonHitu@users.noreply.github.com>
2020-10-16 16:42:00 +0700

    Spring Hibernate Example

---
+++ SpringHibernateExample/src/main/java/com/spring/demo/service/PhoneService.java
@@ -0,0 +1,20 @@
+package com.spring.demo.service;
+
+import java.util.List;
+
+import com.spring.demo.model.Phone;
+
+public interface PhoneService {
+	
+	//public void setPhone(Phone p);
+	
+	public void addPhone(Phone p);
+
+	public void updatePhone(Phone p);
+
+	public List<Phone> listPhones();
+
+	public Phone getPhoneById(int id);
+
+	public void removePhone(int id);
+}
